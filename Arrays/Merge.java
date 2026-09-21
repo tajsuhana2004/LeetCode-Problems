722c4dba306264c7d06c232ecb770dc60b1f2e79
Suhana Taj <tajsuhana2004@gmail.com>
2026-09-21 17:47:32 +0530

    Add files via upload

---
+++ Arrays/Merge.java
@@ -0,0 +1,34 @@
+/* Question number 88
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. 
+nums2 has a length of n.
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1. */
+
+public class Merge {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int idx = m + n -1;//(last position of A)
+        int i = m -1; //(last real element in A)
+        int j = n -1; //(last element in B)
+
+        while(i >=0 && j >=0){
+            if(nums1[i] >= nums2[j]){
+                nums1[idx--] = nums1[i--];// A is bigger, put A at back
+            } else {
+                nums1[idx--] = nums2[j--];/// B is bigger, put B at back
+            }
+        }
+        while(j >=0){
+            nums1[idx--] = nums2[j--];
+        }
+    }
+    
+}
